068e515c594b555798a512b77298eb2226dc789a
Krzysztof Garmulewicz <garmulewicz@me.com>
2025-07-06 23:23:43 +0200

    Added addView prototype

---
+++ RemindAir25/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  RemindAir25
+//
+//  Created by Krzysztof Garmulewicz on 06/07/2025.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
