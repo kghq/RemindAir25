e9e8e388a191730f2d25694003f422693c483f8d
Krzysztof Garmulewicz <garmulewicz@me.com>
2025-07-06 21:36:35 +0200

    Initial Commit

---
+++ RemindAir25/RemindAir25App.swift
@@ -0,0 +1,17 @@
+//
+//  RemindAir25App.swift
+//  RemindAir25
+//
+//  Created by Krzysztof Garmulewicz on 06/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct RemindAir25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
