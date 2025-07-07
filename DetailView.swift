0211ed1208c28528c3c482a81ca57b5cb4098e1e
Krzysztof Garmulewicz <garmulewicz@me.com>
2025-07-07 13:42:25 +0200

    Added DetailView, started meddling with navigation.

---
+++ DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  RemindAir25
+//
+//  Created by Krzysztof Garmulewicz on 07/07/2025.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
